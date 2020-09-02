134ded5902cdc3640de002f06548b9e30d702b52
faisal12ka4 <57749037+faisal12ka4@users.noreply.github.com>
2020-09-02 01:46:12 -0400

    Update code.php

--- school/code.php
+++ school/code.php
@@ -1,15 +1,3 @@
 <?php
-    $name = $_REQUEST["name"];
-    $email = $_REQUEST["email"];
-    $mobile = $_REQUEST["mobile"];
-    $comment = $_REQUEST["comment"];
-
-    $to = "whatfaisal12ka4@gmail.com";
-    $subject = "New Message From John Doe";
-    $body = "abc";
-    $header = "From: $name <$email>";
-
-    $mail = mail($to, $subject, $body, $header);
-
     header("location:index.html");
 ?>
