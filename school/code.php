29c4fe7cfaa39717974117dde4f4be10cc4a3c2c
faisal12ka4 <57749037+faisal12ka4@users.noreply.github.com>
2020-09-02 01:26:41 -0400

    Update code.php

--- school/code.php
+++ school/code.php
@@ -6,7 +6,7 @@
 
     $to = "whatfaisal12ka4@gmail.com";
     $subject = "New Message From John Doe";
-    $body = "<b>Name</b>: $name \n <b>Email</b>: $email \n <b>Mobile No.</b>: $mobile \n <b>Message</b>: $message";
+    $body = "abc";
     $header = "From: $name <$email>";
 
     $mail = mail($to, $subject, $body, $header);
